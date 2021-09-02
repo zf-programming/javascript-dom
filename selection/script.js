7577b941ca54899449b7aa4bd80bf30ca1f2d2e6
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-02 08:02:45 +0700

    getElementsByClassName()

--- selection/script.js
+++ selection/script.js
@@ -11,16 +11,28 @@
 // a.style.backgroundColor = `lightseagreen`;
 // b.style.backgroundColor = `coral`;
 
-// 2. getElementsByTagName() -> HTMLCollection
-const h1 = document.getElementsByTagName(`h1`);
-h1[0].innerHTML = `DOM Selection`;
+// // 2. getElementsByTagName() -> HTMLCollection
+// const h1 = document.getElementsByTagName(`h1`);
+// h1[0].innerHTML = `DOM Selection`;
 
-const p = document.getElementsByTagName(`p`);
-for (let i = 0; i < p.length; i++) {
-  p[i].style.color = `red`;
+// const p = document.getElementsByTagName(`p`);
+// for (let i = 0; i < p.length; i++) {
+//   p[i].style.color = `red`;
+// }
+
+// p[2].innerHTML = `Diubah dengan Javascript`;
+
+// const li2 = document.getElementsByTagName(`li`)[1];
+// li2.innerHTML = `Ubah li ke 2`;
+
+// 3. getElementsByClassName() -> HTMLCollection
+const p1 = document.getElementsByClassName(`p1`);
+p1[0].innerHTML = `P1 Javascript`;
+
+const p2 = document.getElementsByClassName(`p2`)[0];
+p2.innerHTML = `ini Paragraf ke 2`;
+
+const paragraf = document.getElementsByClassName(`paragraf`);
+for (let i = 0; i < paragraf.length; i++) {
+  paragraf[i].style.backgroundColor = `lightgreen`;
 }
-
-p[2].innerHTML = `Diubah dengan Javascript`;
-
-const li2 = document.getElementsByTagName(`li`)[1];
-li2.innerHTML = `Ubah li ke 2`;
