498e2b2719da2ff383c9ae6dd62a5f5df186f933
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-02 16:57:42 +0700

    element.style.<properti-css>

--- manipulation/script.js
+++ manipulation/script.js
@@ -1,9 +1,18 @@
 // DOM Manipulation
 
-// 1. element.innerHTML
-// mengubah isi sebuah elemen / tag
-const judul = document.getElementById(`judul`);
-judul.innerHTML = `Fahrul Zaman`;
+// // 1. element.innerHTML
+// // mengubah isi sebuah elemen / tag
+// const judul = document.getElementById(`judul`);
+// judul.innerHTML = `Fahrul Zaman`;
 
-const sectionA = document.getElementById(`a`);
-sectionA.innerHTML = `<div class="content"><h1>Halo</h1></div>`;
+// const sectionA = document.getElementById(`a`);
+// sectionA.innerHTML = `<div class="content"><h1>Halo</h1></div>`;
+
+// 2. element.style.<property-css>
+const p2 = document.querySelector(`p.paragraf.p2`);
+p2.style.backgroundColor = `lightseagreen`;
+p2.style.fontFamily = `Courier New`;
+
+const sectionB = document.getElementById(`b`);
+sectionB.style.color = `lightblue`;
+sectionB.style.height = `900px`;
