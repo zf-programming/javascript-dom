5901e8785a5fd872a565b8057bd84cf7f61025ab
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-02 07:51:46 +0700

    getElementById

---
+++ selection/script.js
@@ -0,0 +1,12 @@
+// DOM Selection
+
+// 1. getElementById()
+const judul = document.getElementById(`judul`);
+const container = document.getElementById(`container`);
+const a = document.getElementById(`a`);
+const b = document.getElementById(`b`);
+
+judul.innerHTML = "Fahrul Zaman";
+container.style.fontFamily = `Courier New`;
+a.style.backgroundColor = `lightseagreen`;
+b.style.backgroundColor = `coral`;
