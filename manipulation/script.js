4cfa95ad088132ee627ac59a9200715cda045ff4
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-02 16:37:57 +0700

    element.innerHTML

---
+++ manipulation/script.js
@@ -0,0 +1,9 @@
+// DOM Manipulation
+
+// 1. element.innerHTML
+// mengubah isi sebuah elemen / tag
+const judul = document.getElementById(`judul`);
+judul.innerHTML = `Fahrul Zaman`;
+
+const sectionA = document.getElementById(`a`);
+sectionA.innerHTML = `<div class="content"><h1>Halo</h1></div>`;
