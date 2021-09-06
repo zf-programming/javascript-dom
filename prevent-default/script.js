df2a8cfd1b5e216188c96916690ce639c64ad263
fhrlzmn <fhrlzmn26@gmail.com>
2021-09-06 16:26:01 +0700

    Prevent Default contoh penggunaan

--- prevent-default/script.js
+++ prevent-default/script.js
@@ -1,38 +1,12 @@
-// // DOM Traversal
+// Prevent Default
+// Menghentikan aksi default element
 
-// // seleksi elemen
-// const close = document.querySelector("span.close");
-// // const card = document.querySelector("div.card");
-
-// close.addEventListener("click", function () {
-//   // card.style.display = "none";
-//   this.parentNode.style.display = "none"; // DOM traversal
-// });
-
-// // // Jika elemen yang sama banyak (card nya banyak)
-const close = document.querySelectorAll(".close"); // nodelist
-// // add event ke close dengan for
-// for (let i = 0; i < close.length; i++) {
-//   close[i].addEventListener("click", function (e) {
-//     console.log(e);
-//     // this.parentNode.style.display = "none";
-//     e.target.parentNode.style.display = "none";
-//   });
-// }
+const close = document.querySelectorAll("a.close");
 
 // dengan forEach
 close.forEach(function (element) {
   element.addEventListener("click", function (e) {
+    e.preventDefault(); // hentikan default method tag <a>
     e.target.parentElement.style.display = "none";
   });
 });
-
-// DOM Traversal Method
-const nama = document.querySelector("span.nama");
-console.log(nama.parentElement); // elemen parent
-console.log(nama.parentNode); // node parent
-console.log(nama.nextSibling); // next Node
-console.log(nama.nextElementSibling); // next Element
-console.log(nama.previousSibling); // previous Node
-console.log(nama.previousElementSibling); // previous Element
-console.log("ok");
